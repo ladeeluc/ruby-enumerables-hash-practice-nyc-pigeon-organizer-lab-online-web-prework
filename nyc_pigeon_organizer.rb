7f1ae3e6db1526485ff841c7a282124138590f88
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 23:14:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,6 +6,7 @@ def nyc_pigeon_organizer(data) # given method and arg. with parameter
   new_hash = {} # created empty hash for new hash
   pp new_hash
   data.each do |attribute, attribute_values| # takes each element of an array
+    pp attribute
     #creating a place holder. retruns orginal unchanged object
     attribute_values.each do |feature_name, pigeons|#takes each element of an array
       #creating a place holder. retruns orginal unchanged object
