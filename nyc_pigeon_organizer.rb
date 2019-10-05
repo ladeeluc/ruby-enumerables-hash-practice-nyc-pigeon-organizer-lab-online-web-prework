e00905a9d96e793eaed18c148f646626aa7b55c3
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 23:48:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data) # given method and arg. with parameter
   pig_hash = {} # created empty hash for new hash
   data.each do |attribute, attribute_values| # takes each element of the  whole array
     #creating a place holder. retruns orginal unchanged hash. goes through entire nested hash to pull out first key of each
-    # of th each of the nested keys with their values. and returns results of :color, :gender ,:lives
+    # each of the nested keys with their values. iterates these keys :color, :gender ,:lives
     attribute_values.each do |feature_name, pigeons|#takes each element of an array
-      #creating a place holder. retruns orginal unchanged hash. goes through the the third key
+      #creating a place holder. retruns orginal unchanged hash. goes through the array of feature_name and pigeons
       # to pull out the values for each key
       pigeons.each do |pigeon|#takes each element of an array
           if pig_hash[pigeon] == nil
