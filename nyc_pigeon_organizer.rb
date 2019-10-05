b11559d5b8ec63abef2bca84e75f80df4c7c3112
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 21:20:44 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+def nyc_pigeon_organizer(data) # given method
+  new_hash = {} # created empty hash for new hash
+  data.each do |attributes,names| # takes each element of an array
+    names.each do |feature,home|
+      home.each do |birds|
+        #if !birds| do
+          #new_hash[birds] = {}
+        end
+    #names = {
+      #pigeon_list:"Theo","Peter Jr.","Lucky","ms_k","Queenie","Andrew","Alex"
+end
+end
+end
+
 end
