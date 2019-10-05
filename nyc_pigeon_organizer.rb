12ee35abf8324fadfb7c5029c62d30555c1d5b16
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 23:58:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,11 +6,12 @@ def nyc_pigeon_organizer(data) # given method and arg. with parameter
     #creating a place holder. retruns orginal unchanged hash. goes through entire nested hash to pull out first key of each
     # each of the nested keys with their values. iterates these keys :color, :gender ,:lives
     attribute_values.each do |feature_name, pigeons|#takes each element of an array
-      #creating a place holder. retruns orginal unchanged hash. goes through the array of feature_name and pigeons
-      # to pull out the values for each key
-      pigeons.each do |pigeon|#takes each element of an array
-          if pig_hash[pigeon] == nil
-            pig_hash[pigeon] = {}
+      #creating a place holder. retruns orginal unchanged hash. iterates through the array of feature_name and pigeons
+      # to pull out the values which are the names of the pigeons in a string for each key
+      pigeons.each do |pigeon|# makes the string of  names
+          if pig_hash[pigeon] == nil #asks if not stringthere. if it not there then it
+            # creates an empty hash
+            pig_hash[pigeon] = {} # creates empty hash for every bird name in values for all keys a empty array
           end
           if pig_hash[pigeon][attribute] == nil
             pig_hash[pigeon][attribute] = []
