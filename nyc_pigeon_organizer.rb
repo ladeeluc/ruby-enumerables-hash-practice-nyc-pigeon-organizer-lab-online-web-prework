f9d5e7963dbcb0c9b0cf6b353d5de659371fc293
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-05 23:07:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,13 @@
 require 'pry'
-def nyc_pigeon_organizer(data) # given method
+def nyc_pigeon_organizer(data) # given method and arg. with parameter
   new_hash = {} # created empty hash for new hash
-  data.each do |attributes,name| # takes each element of an array
+  data.each do |attribute, attribute_values| # takes each element of an array
     #creating a place holder. retruns orginal unchanged object
-    name.each do |feature,home|
-      home.each do |birds|
-        if !new_hash[birds]# bang method forces new hash to Iterate
-          # data into new array
-          new_hash[birds] = {}
-          new_hash [birds][attributes] = []
-          binding.pry
-        end
-        #if !birds| do
-          #new_hash[birds] = {}
-        end
-    #names = {
-      #pigeon_list:"Theo","Peter Jr.","Lucky","ms_k","Queenie","Andrew","Alex"
+    attribute_values.each do |feature_name, pigeons|#takes each element of an array
+      #creating a place holder. retruns orginal unchanged object
+      pigeons.each do |pigeon|#takes each element of an array
+          new_hash[pigeon][attribute].push(feature_name)
       end
-
     end
-
+  end
 end
